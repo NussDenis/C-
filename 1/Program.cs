6759e1b68caea1e204526d9bfbf1a969d40f139a
Denis <nuss_denis@mail.ru>
2022-11-12 23:31:29 +0700

    Выполнил 2 задание

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+//Тип данных Имя переменной = значение переменной
+int number = 5;
+int numbersecond = 9;
+int result = number * number;
+Console.WriteLine($"{number} * {number} {result}");
+Console.WriteLine(numbersecond * numbersecond);
+
+int number = 20;
+if (number > 10)
+{
+    Console.WriteLine("Число больше 10");
+}
+else
+{
+    Console.WriteLine("Число не больше 10");
+}
+
+int number = 5;
+
+while (number < 10)
+{
+    Console.WriteLine(number);
+    number++;
+}
+
+int a = 2;
+int b = 10;
+int result = a * a;
+if(b == result)
+{
+    Console.WriteLine("Число а является квадратом числа b");
+}
+else
+{
+    Console.WriteLine("Число а НЕ является квадратом числа b");
+}
+
+Console.WriteLine("Введите число от 1 до 7");
+int result = Convert.ToInt32(Console.ReadLine());
+if(result == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(result == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(result == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(result == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(result == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(result == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(result == 7)
+{
+    Console.WriteLine("Воскресень");
+}
+else
+{
+    Console.WriteLine("неверный ввод!");
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintPower(number);
+
+void PrintPower(int number)
+{
+    Console.WriteLine($"{number} * {number} = {number * number}");
+
+}
+*/
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+number = Math.Abs(number);
+int count = -number;
+while(count <= number)
+{
+    Console.Write(count + " ");
+    count++;
+}
+Console.WriteLine();
+
--- Семинар_1/2 задание/Program.cs
+++ Семинар_1/2 задание/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+Console.WriteLine("Введите 3 любых числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = b;
+int min = c;
+if(a > max){
+    max = a;
+    min = b;
+}
+else{
+    max = b;
+    min = a;
+}
+if(c > max)
+{
+    max = c;
+}
+else if(c < min){
+    min = c;
+}
+Console.WriteLine($"{max} = max");
+Console.WriteLine($"{min} = min");
