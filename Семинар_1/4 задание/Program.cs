ebf5cb00e96d7b205fc67a862d8c4127dad64797
Denis <nuss_denis@mail.ru>
2022-11-13 00:45:59 +0700

    Сделал четвертое задание на С#

---
+++ Семинар_1/4 задание/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+while(result <= number){
+    if(result%2 == 0){
+        Console.Write(result +" ");
+    }
+result++;
+}
