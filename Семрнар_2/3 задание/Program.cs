8bed833d1751ef62a65437cbcf8b6516f174a22c
Denis <nuss_denis@mail.ru>
2022-11-13 23:10:57 +0700

    Выполнил третье задание 2-го семинара

---
+++ Семрнар_2/3 задание/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void Function()
+{
+    Console.Write("Введите день недели: ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    if (result == 1 || result < 6)
+    {
+        Console.WriteLine("Это будний день");
+    }
+     else if (result == 6 || result == 7)
+    {
+        Console.WriteLine("Это выходной день");
+    }
+       else
+    {
+        Console.WriteLine("неверный ввод!");
+    }
+}
+Function();
