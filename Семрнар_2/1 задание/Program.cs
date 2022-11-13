12ff143f2773b3d6f6ae824309e735f4d4ad911c
Denis <nuss_denis@mail.ru>
2022-11-13 21:47:36 +0700

    Выполнил первое задание 2-го семинара

---
+++ Семрнар_2/1 задание/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+void Function()
+{
+    Random rand = new Random();
+    int number = rand.Next(100, 1000);
+    Console.WriteLine($"Случайное число: {number}");
+    int firstnumber = number / 10;
+    Console.WriteLine($"Вторая цифра случайного числа: {firstnumber % 10}");
+}
+Function();
