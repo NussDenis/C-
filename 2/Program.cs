1d37d9dbb86b3fc4eed2d3535fbd47e189b289a1
Denis <nuss_denis@mail.ru>
2022-11-23 21:51:36 +0700

    Создал папку с 4 семинаром

--- 2/Program.cs
+++ 2/Program.cs
@@ -46,34 +46,124 @@
 
 // void Zadacha()
 // {
-    // Console.WriteLine("Введите число");
-    // int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
 
-    // if (a % 7 == 0 && a % 23 == 0)
-    // {
-    //     Console.WriteLine("Введенное число кратно 7 и 23");
-    // }
-    // else
-    // {
-    //     Console.WriteLine("Не кратно одному из числу");
-    // }
+// if (a % 7 == 0 && a % 23 == 0)
+// {
+//     Console.WriteLine("Введенное число кратно 7 и 23");
+// }
+// else
+// {
+//     Console.WriteLine("Не кратно одному из числу");
+// }
 
 
-    // Задача 5.
-void Zadacha()
-{
-    Console.WriteLine("Введите первое число");
-    int a = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите второе число");
-    int b = Convert.ToInt32(Console.ReadLine());
+// Задача 5.
+// void Zadacha()
+// {
+//     Console.WriteLine("Введите первое число");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите второе число");
+//     int b = Convert.ToInt32(Console.ReadLine());
 
-    if (a * a == b || b * b == a)
-    {
-        Console.WriteLine("Число является квадратом введеного числа");
-    }
-    else
-    {
-        Console.WriteLine("Число не является квадратом введеного числа");
-    }
-}
-Zadacha();
+//     if (a * a == b || b * b == a)
+//     {
+//         Console.WriteLine("Число является квадратом введеного числа");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число не является квадратом введеного числа");
+//     }
+// }
+// Zadacha();
+
+// void Zadacha17()
+// {
+//     Random rand = new Random();
+//     int x = rand.Next(-10, 11);
+//     int y = rand.Next(-10, 11);
+//     Console.WriteLine($"A {x}, {y})");
+
+//     if (x > 0 && y > 0)
+//     {
+//         Console.WriteLine("1-я четверть");
+//     }
+//     else if (x < 0 && y > 0)
+//     {
+//         Console.WriteLine("2-я четверть");
+//     }
+//     else if (x < 0 && y < 0)
+//     {
+//         Console.WriteLine("3-я четверть");
+//     }
+//     else if (x > 0 && y < 0)
+//     {
+//         Console.WriteLine("4-я четверть");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Точка лежит на координатной прямой");
+//     }
+// }
+// Zadacha17();
+
+// void Zadacha18()
+// {
+//     Console.WriteLine("Введите номер четверти");
+//     int a = Convert.ToInt32(Console.ReadLine());
+
+//     if (a == 1)
+//     {
+//         Console.WriteLine("В данной четверти Х>0 и Y>0");
+//     }
+//     else if (a == 2)
+//     {
+//         Console.WriteLine("В данной четверти Х<0 и Y>0");
+//     }
+//     else if (a == 3)
+//     {
+//         Console.WriteLine("В данной четверти Х<0 и Y<0");
+//     }
+//     else if (a == 4)
+//     {
+//         Console.WriteLine("В данной четверти Х>0 и Y<0");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Неверно введенная четверть!!!");
+//     }
+// }
+// Zadacha18();
+
+// void Zadacha21()
+// {
+//     Console.WriteLine("Введите координаты 1-ой точки");
+//     double x1 = Convert.ToInt32(Console.ReadLine());
+//     double y1 = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите координату 2-ой точки");
+//     double x2 = Convert.ToInt32(Console.ReadLine());
+//     double y2 = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine($"Расстояние = {Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2))}");
+// }
+// Zadacha21();
+
+// void Zadacha22()
+// {
+//     Console.WriteLine("Введите любое целое число");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     int b = 1;
+//     while (b <= a)
+//     {
+//         Console.WriteLine($"{b} * {b} = {(Math.Pow(b, 2))}");
+//         b++;
+//     }
+// }
+// Zadacha23();
+
+// void Zadacha23()
+// {
+
+// }
+// Zadacha23();
+
---
+++ 4/Program.cs
@@ -0,0 +1,75 @@
+﻿// int SumNumber(int number)
+// {
+//     int sum = 0;
+//     while (number > 0)
+//     {
+//         sum += number;
+
+//     }
+// }
+// void Zadacha24()
+// {
+//     Random rand = new Random();
+//     int number = rand.Next(1, 20);
+//     Console.WriteLine("Введено число " + number);
+//     int counter = 1;
+//     int sum = 0;
+//     while (sum <= number)
+//     {
+//         Console.Write($"{sum} + {counter} =  ");
+//         sum = sum + counter;
+//         Console.WriteLine(sum);
+//         counter++;
+//     }
+// }
+// Zadacha24();
+
+
+// void Zadacha26()
+// {
+//     Random rand = new Random();
+//     int number = rand.Next(1, 100000);
+//     Console.WriteLine("Введено число - " + number);
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         number = number / 10;
+//         result++;
+//     }
+//     Console.Write("В веденном числе цифр - " + result);
+// }
+// Zadacha26();
+
+// void Zadacha28()
+// {
+//     Random rand = new Random();
+//     int number = rand.Next(1, 25);
+//     Console.WriteLine("Введено число " + number);
+//     int mult = 1;
+//     int count = 1;
+//     while (count <= number)
+//     {
+//         mult = mult * count;
+//         Console.WriteLine(count);
+//         count++;
+//     }
+//      Console.WriteLine($"Произведение равно {mult}");
+// }
+// Zadacha28();
+
+
+void Zadacha30()
+{
+    int size = 8;
+    Random rand = new Random();
+    int[] numbers = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = rand.Next(0, 2);
+    }
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+}
+Zadacha30();
