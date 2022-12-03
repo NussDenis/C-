6552dca881b35ea4107f59e2060524b471ca3c4a
Denis <nuss_denis@mail.ru>
2022-12-03 22:06:51 +0700

    Выполнил ДЗ 7 семинара

---
+++ 6/Program.cs
@@ -0,0 +1,133 @@
+﻿void FillArray(int[,] nums, int minValue, int maxValue)
+{
+    maxValue++;
+    Random rand = new Random();
+    int rows = nums.GetLength(0);
+    int columns = nums.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            nums[i, j] = rand.Next(minValue, maxValue);
+        }
+    }
+}
+void PrintArray(int[,] nums)
+{
+    int rows = nums.GetLength(0);
+    int columns = nums.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{nums[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// Задача 48:
+// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: A = m+n. 
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void Zadacha48()
+{
+    int rows = 8;
+    int columns = 8;
+    int[,] numbers = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = i + j;
+            Console.Write($"{numbers[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+// Zadacha48();
+
+
+// Задача 49:
+// Задайте двумерный массив. Найдите элементы, у 
+// которых оба индекса чётные, и замените эти элементы на их 
+// квадраты.
+
+void Zadacha49()
+{
+    int rows = 4;
+    int columns = 4;
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers, 0, 10);
+    PrintArray(numbers);
+    Console.WriteLine();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                numbers[i, j] = (numbers[i, j]) * (numbers[i, j]);
+                Console.Write($"{numbers[i, j]}\t");
+            }
+            else Console.Write($"{numbers[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Zadacha49();
+
+// Задача 51:
+// Задайте двумерный массив. Найдите сумму 
+// элементов, находящихся на главной диагонали (с индексами (0,0); 
+// (1;1) и т.д.
+
+void Zadacha51()
+{
+    int rows = 4;
+    int columns = 4;
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers, 0, 10);
+    PrintArray(numbers);
+    int sum = 0;
+    Console.WriteLine();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i == j)
+            {
+                sum += numbers[i, j];
+            }
+        }
+    }
+    Console.WriteLine("Сумма элементов по диагонале = " + sum);
+}
+// Zadacha51();
+
+void Zadacha51_2()
+{
+    int rows = 8;
+    int columns = 4;
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers, 0, 10);
+    PrintArray(numbers);
+    int min = Math.Min(rows, columns);
+    int sum = 0;
+    Console.WriteLine();
+    for (int i = 0; i < min; i++)
+    {
+        {
+            sum += numbers[i,i];
+        }
+    }
+Console.WriteLine("Сумма элементов по диагонале = " + sum);
+}
+Zadacha51_2();
---
+++ Семинар_7/Program.cs
@@ -0,0 +1,118 @@
+﻿void FillArray(int[,] nums, int minValue, int maxValue)
+{
+    maxValue++;
+    Random rand = new Random();
+    int rows = nums.GetLength(0);
+    int columns = nums.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            nums[i, j] = rand.Next(minValue, maxValue);
+        }
+    }
+}
+void PrintArray(int[,] nums)
+{
+    int rows = nums.GetLength(0);
+    int columns = nums.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{nums[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void Zadacha47()
+{
+    int rows = 4;
+    int columns = 4;
+    double[,] numbers = new double[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = Math.Round((rand.NextDouble() * 100 - 50), 1);
+            Console.Write($"{numbers[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Задача 50. Напишите программу, которая на вход принимает индексы элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1, 3 -> 3
+
+void Zadacha50()
+{
+    int rows = 5;
+    int columns = 5;
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers, -100, 100);
+    PrintArray(numbers);
+    Console.WriteLine();
+    Console.Write("Введите значение строки - ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите значение столбца - ");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (i == n && j == m)
+            {
+                Console.Write($"Значение выбранного элемента в массиве = {numbers[i, j]}");
+            }
+        }
+    }
+    if (rows <= n && columns <= m) Console.WriteLine("В массиве такой элемент отсутствует");
+}
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 5 3
+// 8 4 6 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 6; 3.
+
+void Zadacha52()
+{
+    int rows = 4;
+    int columns = 4;
+    int[,] numbers = new int[rows, columns];
+    double sum = 0;
+    FillArray(numbers, 0, 20);
+    PrintArray(numbers);
+    Console.WriteLine();
+    for (int j = 0; j < columns; j++)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            sum += numbers[i, j];
+        }
+        Console.WriteLine($"Среднееарифметическое значение {j + 1} столбца = {sum / rows}");
+        sum = 0;
+    }
+}
+
+
+// Zadacha47();
+// Zadacha50();
+// Zadacha52();
